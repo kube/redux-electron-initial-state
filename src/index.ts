19513ad07bc7de45fc7d00ba24305ab05a8d2bf2
Chris Feijoo <christophe@feijoo.fr>
2017-03-26 19:37:01 +0200

    Handle undefined Initial State

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,23 @@ import { StoreEnhancer } from 'redux'
 const REDUX_INITIAL_STATE = '@@REDUX_INITIAL_STATE'
 
 export const setWindowInitialState =
-  <S>(window: Electron.BrowserWindow, state: S) =>
+  <S>(window: Electron.BrowserWindow, state: S) => {
     (window as any)[REDUX_INITIAL_STATE] = JSON.stringify(state)
+  }
 
 export const initalStateEnhancer: StoreEnhancer<any> =
   createStore => reducer => {
     const window: any = remote.getCurrentWindow()
-    const initialState = JSON.parse(window[REDUX_INITIAL_STATE])
-    return createStore(reducer, initialState)
+    const rawInitialState = window[REDUX_INITIAL_STATE]
+
+    if (rawInitialState === undefined)
+      return createStore(reducer)
+
+    try {
+      const initialState = JSON.parse(rawInitialState)
+      return createStore(reducer, initialState)
+    }
+    catch (error) {
+      throw new Error('Redux Initial State: Invalid State')
+    }
   }
